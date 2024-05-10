dcd63f5072d6d70f120a1cf70d03899bb387d581
itscsp <chethanspoojary1@gmail.com>
2024-05-10 13:01:50 +0530

    Building cart page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,9 +8,11 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
+        <Link to='/'>
         <img src={assets.logo} alt="main-logo" className='logo' />
+        </Link>
         <ul className="navbar-menu">
-            <Link onClick={() => {setMenu('home')}} className={menu === 'home' ? 'active' : ''} >Home</Link>
+            <Link  to='/' onClick={() => {setMenu('home')}} className={menu === 'home' ? 'active' : ''} >Home</Link>
             <a href='#explore-menu' onClick={() => {setMenu('menu')}} className={menu === 'menu' ? 'active' : ''} >Menu</a>
             <a href='#app-download' onClick={() => {setMenu('mobile-app')}} className={menu === 'mobile-app' ? 'active' : ''} >Mobile-app</a>
             <a href='#footer' onClick={() => {setMenu('contact-us')}} className={menu === 'contact-us' ? 'active' : ''} >Contact Us</a>
@@ -18,7 +20,9 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
+                <Link to='/cart'>
                 <img src={assets.basket_icon} alt="" />
+                </Link>
                 <div className="dot">
 
                 </div>
