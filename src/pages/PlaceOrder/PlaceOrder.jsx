16add1f9691c733518e93ffed920979ae825fe17
itscsp <chethanspoojary1@gmail.com>
2024-06-08 23:05:45 +0530

    Working place order form

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,18 @@
 import "./PlaceOrder.css";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import { Link } from "react-router-dom";
 
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+
+  })
+
   return (
     <form className="place-order">
       <div className="place-order-left">
